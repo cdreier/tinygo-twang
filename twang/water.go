deff62eba528df29360e31d6f507d4bd38c98e65
Christian Dreier <dreier.christian@gmail.com>
2024-03-23 16:45:32 +0100

    feat: moving water in directions

--- twang/water.go
+++ twang/water.go
@@ -7,13 +7,14 @@ import (
 type Water struct {
 	size             int
 	index            int
+	direction        int
 	power            int
 	powerCount       int
 	colorOffsetCount int
 	colors           []color.RGBA
 }
 
-func NewWater(size int, position int) *Water {
+func NewWater(size int, position int, direction int) *Water {
 
 	colors := make([]color.RGBA, size)
 	for i := 0; i < size; i++ {
@@ -25,20 +26,23 @@ func NewWater(size int, position int) *Water {
 	}
 
 	return &Water{
-		size:   size,
-		index:  position,
-		power:  2,
-		colors: colors,
+		size:      size,
+		index:     position,
+		power:     2,
+		colors:    colors,
+		direction: direction,
 	}
 }
 
 func (w *Water) Update() {
 	w.colorOffsetCount++
-	if w.colorOffsetCount%4 == 0 {
+	if w.colorOffsetCount%5 == 0 {
 		// shift colors
-		var tmp color.RGBA
-		tmp, w.colors = w.colors[0], w.colors[1:]
-		w.colors = append(w.colors, tmp)
+		if w.direction > 0 {
+			w.colors = append(w.colors[1:], w.colors[0])
+		} else {
+			w.colors = append([]color.RGBA{w.colors[len(w.colors)-1]}, w.colors[:len(w.colors)-1]...)
+		}
 	}
 }
 
@@ -55,6 +59,6 @@ func (w *Water) Render(index int, colors []color.RGBA) bool {
 func (w *Water) Intersection(g *Game) {
 	w.powerCount++
 	if w.powerCount%w.power != 0 && inRange(g.Player.index, w.index, w.index+w.size) {
-		g.Player.index--
+		g.Player.index -= w.direction
 	}
 }
